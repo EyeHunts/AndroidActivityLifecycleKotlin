708d142f0990e6406dcbc1208f06c503bdb74da0
Rohit <rkkanojia7@gmail,com>
2018-06-22 11:21:41 +0530

    Initial commit

---
+++ app/src/main/java/in/eyehunt/androidactivitylifecyclekotlin/MainActivity.kt
@@ -0,0 +1,48 @@
+package `in`.eyehunt.androidactivitylifecyclekotlin
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        print("onCreate")
+    }
+
+    override fun onStart() {
+        super.onStart()
+        print("onStart")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        print("onResume")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        print("onPause")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        print("onStop")
+    }
+
+    override fun onRestart() {
+        super.onRestart()
+        print("onRestart")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        print("onDestroy")
+    }
+
+    fun print(msg: String){
+        Log.d("Activity State ",msg)
+    }
+}
